7278e9d78ed73d0ea22def26eb819ae6c6397ca8
saisimha97 <vsaisimha@gmail.com>
2020-01-03 22:20:57 +0530

    removing

--- camera.py
+++ camera.py
@@ -8,6 +8,7 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
 
+
     def get_frame(self):
         success, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpg', image)
