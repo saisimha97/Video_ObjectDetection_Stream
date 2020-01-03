d5532dfed59ab828e10ebf8687fd06996c0cca3a
saisimha97 <vsaisimha@gmail.com>
2020-01-03 21:48:11 +0530

    Commented

--- camera.py
+++ camera.py
@@ -7,7 +7,8 @@ class VideoCamera(object):
     
     def __del__(self):
         self.video.release()
-    
+
+# Helo    
     def get_frame(self):
         success, image = self.video.read()
         ret, jpeg = cv2.imencode('.jpg', image)
